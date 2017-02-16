1ad5bac5999248774cf732175a29695f83b5e315
NinaYoung <18612498868@163.com>
2017-02-16 17:32:45 +0800

    lesson1

---
+++ src/main/java/com/learning/test/framework/webframeworktest/App.java
@@ -0,0 +1,13 @@
+package com.learning.test.framework.webframeworktest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
